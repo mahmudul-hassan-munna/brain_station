78d7b53bee4fc81571116a2e17cdd9c2ac37db88
mahmudul-hassan-munna <hassan@teknordic.com>
2020-05-28 22:21:48 +0600

    first commit

--- task_2.php
+++ task_2.php
@@ -2,59 +2,6 @@
 require_once "database.php";
 
 
-// if ($result = $mysqli -> query("SELECT cr.*,c.Name 
-// FROM `catetory_relations` cr 
-// inner join category c on c.id=cr.`ParentcategoryId`
-// order by `ParentcategoryId` asc")) {
-//   //echo "Returned rows are: " . $result -> num_rows;
-//   // Free result set
-
-//    echo "<ul>";
-                                
-
-//   while($row = mysqli_fetch_array($result)){
-
-
-
-
-
-  
-//   	echo "<li>".$row['Name']."</li>";
-
-//     $parent = $row['categoryId'];
-
-//     echo "SELECT cr.*,c.Name 
-//     FROM `catetory_relations` cr 
-//     inner join category c on c.id=cr.`ParentcategoryId`
-//     where cr.ParentcategoryId = $parent";
-
-
-//     $result_child = $mysqli -> query("SELECT cr.*,c.Name 
-//     FROM `catetory_relations` cr 
-//     inner join category c on c.id=cr.`ParentcategoryId`
-//     where cr.ParentcategoryId = $parent");
-
-
-//     echo "<ul>";
-//     while($row_child = mysqli_fetch_array($result_child)){
-//       echo "<li>".$row['Name']."</li>";
-
-//     }
-//     echo "</ul>";
-//     //echo $result -> num_rows;
-
-
-  
-//   }
-
-//   echo "</ul>";
-
-
-//   $result -> free_result();
-//   //print_r($result);die();
-// }
-
-
 
 function get_menu_tree($parent_id) 
 {
